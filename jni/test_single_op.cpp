9dad5b856154fca30225cca45820fc05e4b942a8
niuwei22007 <371073260@qq.com>
2020-11-09 16:53:32 +0800

    fix

--- jni/test_single_op.cpp
+++ jni/test_single_op.cpp
@@ -6,7 +6,7 @@ bool CheckResult() {
     return true;
 }
 
-void Test(const TestCase &test) {
+void Test(const TestCase& test) {
     string modelName = "/data/local/tmp/output/" + test.caseName + ".om";
     ge::Graph irGraph = ge::Graph("ir_graph");
     cout << "============= CaseName: " << test.caseName << endl;
@@ -32,20 +32,20 @@ void Test(const TestCase &test) {
     }
     PrintTensorData<float>(inputTensors[0], 0, 32);
     int i = 0;
-    for (const shared_ptr<hiai::AiTensor> &tensor : outputTensors) {
+    for (const shared_ptr<hiai::AiTensor>& tensor : outputTensors) {
         PrintTensorData<float>(tensor, 0, 32);
         SaveTensorData<float>(tensor, "/data/local/tmp/output/output_" + to_string(i++) + ".bin");
     }
     cout << "-------------" << test.caseName << " -------- " << CheckResult() << endl;
 }
 
-bool BuildSqrtGraph(ge::Graph &graph) {
+bool BuildSqrtGraph(ge::Graph& graph) {
     auto data = ge::op::Data("data");
     ge::TensorDesc inputDesc(ge::Shape({16, 512, 1, 1}));
     data.update_input_desc_x(inputDesc);
 
     auto sqrt = ge::op::Sqrt("sqrt")
-            .set_input_x(data);
+        .set_input_x(data);
 
     vector<ge::Operator> inputs;
     inputs.push_back(data);
@@ -55,7 +55,7 @@ bool BuildSqrtGraph(ge::Graph &graph) {
     return true;
 }
 
-bool BuildConvTransposeGraph(ge::Graph &graph) {
+bool BuildConvTransposeGraph(ge::Graph& graph) {
     auto data = ge::op::Data("data");
     ge::TensorDesc inputDesc(ge::Shape({1, 8, 864, 480}));
     data.update_input_desc_x(inputDesc);
@@ -68,33 +68,33 @@ bool BuildConvTransposeGraph(ge::Graph &graph) {
     {
         hiai::TensorDesc outDesc(ge::Shape({4}), ge::FORMAT_NCHW, ge::DT_INT32);
         std::vector<int32_t> outShapeValue{
-                (int32_t) inputDesc.GetShape().GetDim(0),
-                (int32_t) inputDesc.GetShape().GetDim(1),
-                (int32_t) inputDesc.GetShape().GetDim(2) * 2,
-                (int32_t) inputDesc.GetShape().GetDim(3) * 2,
+            (int32_t)inputDesc.GetShape().GetDim(0),
+            (int32_t)inputDesc.GetShape().GetDim(1),
+            (int32_t)inputDesc.GetShape().GetDim(2) * 2,
+            (int32_t)inputDesc.GetShape().GetDim(3) * 2,
         };
-        SetConstData(outputShape, outDesc, (uint8_t *) outShapeValue.data(), outShapeValue.size() * sizeof(uint32_t));
+        SetConstData(outputShape, outDesc, (uint8_t*)outShapeValue.data(), outShapeValue.size() * sizeof(uint32_t));
 
         hiai::Shape filterShape = ge::Shape({8, 1, 4, 4});
         hiai::Shape biasShape = ge::Shape({1, 1, 1, 1});
 
         hiai::TensorDesc convWeightDesc(filterShape, ge::FORMAT_NCHW, ge::DT_FLOAT);
         vector<float> convWeightValue(Prod(filterShape.GetDims()), 1);
-        SetConstData(filter, convWeightDesc, (uint8_t *) convWeightValue.data(),
+        SetConstData(filter, convWeightDesc, (uint8_t*)convWeightValue.data(),
                      convWeightValue.size() * sizeof(float));
 
         hiai::TensorDesc convBiasDesc(biasShape, ge::FORMAT_NCHW, ge::DT_FLOAT);
         vector<float> convBiasValue(Prod(biasShape.GetDims()), 1);
-        SetConstData(bias, convBiasDesc, (uint8_t *) convBiasValue.data(), convBiasValue.size() * sizeof(float));
+        SetConstData(bias, convBiasDesc, (uint8_t*)convBiasValue.data(), convBiasValue.size() * sizeof(float));
 
         deconvOp.set_input_output_shape(outputShape)
-                .set_input_filter(filter)
-                .set_input_x(data)
-                .set_attr_dilations({1, 1})
-                .set_attr_strides({2, 2})
-                .set_attr_groups(1)
-                .set_attr_pad_mode("SAME")
-                .set_attr_pads({0, 0, 0, 0});
+            .set_input_filter(filter)
+            .set_input_x(data)
+            .set_attr_dilations({1, 1})
+            .set_attr_strides({2, 2})
+            .set_attr_groups(1)
+            .set_attr_pad_mode("SAME")
+            .set_attr_pads({0, 0, 0, 0});
     }
 
     std::vector<ge::Operator> inputs{data};
@@ -103,7 +103,7 @@ bool BuildConvTransposeGraph(ge::Graph &graph) {
     return true;
 }
 
-bool BuildResizeBilinearGraph(ge::Graph &graph) {
+bool BuildResizeBilinearGraph(ge::Graph& graph) {
     auto data = ge::op::Data("data");
     ge::TensorDesc inputDesc(ge::Shape({1, 32, 192, 192}));
     data.update_input_desc_x(inputDesc);
@@ -112,14 +112,13 @@ bool BuildResizeBilinearGraph(ge::Graph &graph) {
     {
         hiai::TensorDesc outDesc(ge::Shape({2}), ge::FORMAT_NCHW, ge::DT_INT32);
         std::vector<int32_t> outShapeValue{384, 384};
-        SetConstData(outputShape, outDesc, (uint8_t *) outShapeValue.data(), outShapeValue.size() * sizeof(uint32_t));
+        SetConstData(outputShape, outDesc, (uint8_t*)outShapeValue.data(), outShapeValue.size() * sizeof(uint32_t));
     }
     auto resize = hiai::op::ResizeBilinearV2("resize_v2")
-            .set_input_x(data)
-            .set_input_size(outputShape)
-            .set_attr_align_corners(false)
-            .set_attr_half_pixel_centers(true);
-
+        .set_input_x(data)
+        .set_input_size(outputShape)
+        .set_attr_align_corners(false)
+        .set_attr_half_pixel_centers(true);
 
     std::vector<ge::Operator> inputs{data};
     std::vector<ge::Operator> outputs{resize};
@@ -130,13 +129,13 @@ bool BuildResizeBilinearGraph(ge::Graph &graph) {
 
 using namespace testcase;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     TestCase caseList[] = {
-//            {"sqrt_ir",          BuildSqrtGraph,          false},
-//            {"convtranspose_ir", BuildConvTransposeGraph, false},
-            {"resizebilinearv2_ir", BuildResizeBilinearGraph, false},
+        {"sqrt_ir",             BuildSqrtGraph,           false},
+        {"convtranspose_ir",    BuildConvTransposeGraph,  false},
+        {"resizebilinearv2_ir", BuildResizeBilinearGraph, false},
     };
-    for (const TestCase &tc : caseList) {
+    for (const TestCase& tc : caseList) {
         Test(tc);
     }
     ALOGI("=========== ALL DONE ===========\n");
